133e040b9a401b49bd5f5e8bea05fea44207e459
AllisonB <arnaudallisonb@gmail.com>
2022-05-17 23:43:01 +1000

    added single post components

--- src/components/singlePost/SinglePost.jsx
+++ src/components/singlePost/SinglePost.jsx
@@ -5,13 +5,30 @@ export default function SinglePost() {
     <div className="singlePost">
       <div className="singlePostWrapper">
         <img 
-          src="" 
+          src="https://wallpaperaccess.com/full/625497.jpg" 
           alt="" 
           className="singlePostImg" 
         />
         <h1 className="singlePostTitle">
-          Lorem ipsum dolor sit amet consec.
+          Lorem ipsum dolor, sit amet conse.
+          singlepost
+          <div className="singlePostEdit">
+            <i className="singlePostIcon far fa-edit"></i>
+            <i className="singlePostIcon far fa-trash-alt"></i>
+          </div>
         </h1>
+        <div className="singlePostInfo">
+          <span className="singlePostAuthor">
+            Author: <b>Allison</b>
+          </span>
+          <span className="singlePostDate">1 hour ago</span>
+        </div>
+        <p className="singlePostDesc">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas consequatur placeat explicabo culpa deleniti quasi maiores nesciunt sapiente asperiores officia aperiam nam nobis, hic, ut unde cum. Officiis, ipsa officia!
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas consequatur placeat explicabo culpa deleniti quasi maiores nesciunt sapiente asperiores officia aperiam nam nobis, hic, ut unde cum. Officiis, ipsa officia!
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas consequatur placeat explicabo culpa deleniti quasi maiores nesciunt sapiente asperiores officia aperiam nam nobis, hic, ut unde cum. Officiis, ipsa officia!
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas consequatur placeat explicabo culpa deleniti quasi maiores nesciunt sapiente asperiores officia aperiam nam nobis, hic, ut unde cum. Officiis, ipsa officia!
+        </p>
       </div>
     </div>
   )
